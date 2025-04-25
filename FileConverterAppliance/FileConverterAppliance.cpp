1f1b273aaa44b6ee1368f178d3fd2c8b713ad9a7
Sharkbyteprojects <sharkbyteprojects@gmail.com>
2025-04-25 19:16:17 +0200

    Make Demo

--- FileConverterAppliance/FileConverterAppliance.cpp
+++ FileConverterAppliance/FileConverterAppliance.cpp
@@ -6,6 +6,10 @@ int main(int argc, char* argv[])
 #define sizeString 13
     const char hw[sizeString] = "Hello World!";
     std::cout << hw << "\n";
-    std::cout << base64Codec::encodeByteBlock((const uint8_t*)hw, sizeString - 1) << std::endl;
+    std::cout << base64Codec::encodeByteBlock((const uint8_t*)hw, sizeString - 1) << "\n" << std::endl;
+
+    for (uint16_t i = 1; i < argc; i++) {
+        std::cout << "\"" << argv[i] << "\" : \"" << base64Codec::encodeByteBlock((const uint8_t*)argv[i], strlen(argv[i])) << "\"\n";
+    }
 }
 
